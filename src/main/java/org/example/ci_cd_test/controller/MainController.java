7ed9893bbb572101336953788b6af9c2f0988f32
phuocnb <phuocnb@PhuocNB.local>
2025-09-22 16:55:03 +0700

    add process for another branches

--- src/main/java/org/example/ci_cd_test/controller/MainController.java
+++ src/main/java/org/example/ci_cd_test/controller/MainController.java
@@ -2,6 +2,7 @@ package org.example.ci_cd_test.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.example.ci_cd_test.service.MainService;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,9 +14,17 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("/main")
 public class MainController {
 
+    private final MainService mainService;
+    private final Environment env;
+
     @GetMapping("greeting")
     public ResponseEntity getGreeting(@RequestParam String type) {
-        return ResponseEntity.ok(new MainService().getGreeting(type));
+        return ResponseEntity.ok(mainService.getGreeting(type));
+    }
+
+    @GetMapping("environment")
+    public ResponseEntity getEnvironment() {
+        return ResponseEntity.ok(env.getProperty("value"));
     }
 
 }
